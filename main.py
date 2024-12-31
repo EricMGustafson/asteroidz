7c4324ce18dbb7ecec49a446d2341aeeab0e6371
EricMGustafson <emgustafson22@gmail.com>
2024-12-31 02:30:21 -0700

    Asteroids and grouping

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -15,21 +14,34 @@ def main():
  
   time_clock = pygame.time.Clock()
   dt = 0
+
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-  
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  updatables = pygame.sprite.Group()
+  drawables = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+
+  Player.containers = (updatables, drawables)
+  Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  Asteroid.containers = (asteroids, updatables, drawables)
+
+  AsteroidField.containers = (updatables)
+  AsteroidField()
 
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     
-    player.update(dt)
+    for updatable in updatables:
+      updatable.update(dt)      
       
     screen.fill("black")
+    
+    for drawable in drawables:
+      drawable.draw(screen)
 
-    player.draw(screen)
     pygame.display.flip()
 
     delta = time_clock.tick(60)
